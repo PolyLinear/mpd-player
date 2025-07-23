a8439e4c5f4c976bf0e1cb0a613c1a3e7fbd3422
PolyLinear <bciesla@umass.edu>
2025-07-23 17:06:26 -0400

    added basic error handling

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod mpd;
 
 use mpd::client::MPDClient;
 use std::error::Error;
-
 fn main() -> Result<(), Box<dyn Error>> {
     println!("{:#?}", MPDClient::new("127.0.0.1:6600")?.get_playlist()?);
     Ok(())
--- src/mpd/client.rs
+++ src/mpd/client.rs
@@ -10,6 +10,23 @@ pub struct MPDClient {
     write_con: BufWriter<TcpStream>,
 }
 
+#[derive(Debug)]
+pub enum MPDError {
+    FailedWrite,
+    FailedCommand,
+}
+
+impl std::fmt::Display for MPDError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::FailedWrite => write!(f, "{}", "Failed to write to socket".to_string()),
+            Self::FailedCommand => write!(f, "{}", "Failed to execute the command".to_string()),
+        }
+    }
+}
+
+impl Error for MPDError {}
+
 impl MPDClient {
     pub fn new(addr: &str) -> Result<Self, Box<dyn Error>> {
         let stream = TcpStream::connect(addr)?;
@@ -31,23 +48,36 @@ impl MPDClient {
         })
     }
 
-    //TODO: checking errors is going to be repeated often; implement generic error handling across
-    //commands
-    pub fn get_playlist(&mut self) -> Result<Vec<String>, Box<dyn Error>> {
-        self.write_con.write(b"playlist\n")?;
-        self.write_con.flush()?;
+    fn mpd_command(&mut self, command: &[u8]) -> Result<String, MPDError> {
+        self.write_con
+            .write_all(command)
+            .and_then(|_| self.write_con.flush())
+            .map_err(|_| MPDError::FailedWrite)?;
 
-        let mut iter = self.read_con.by_ref().lines().flatten();
-        if let Some(first) = iter.next() {
-            if first.starts_with("ACK [") {
-                return Err("Command was not executed correctly".into());
-            }
-            let mut result = Vec::with_capacity(16);
-            result.push(first);
-            result.extend(iter.take_while(|line| line != "OK"));
-            return Ok(result);
+        let mut buffer = String::new();
+        self.read_con
+            .read_line(&mut buffer)
+            .map_err(|_| MPDError::FailedCommand)?;
+
+        if buffer.starts_with("ACK [") {
+            return Err(MPDError::FailedCommand);
         }
 
-        Err("Timeout".into())
+        Ok(buffer)
+    }
+
+    pub fn get_playlist(&mut self) -> Result<Vec<String>, MPDError> {
+        let first = self.mpd_command(b"playlist\n")?;
+        let mut playlist = Vec::with_capacity(16);
+        playlist.push(first);
+        playlist.extend(
+            self.read_con
+                .by_ref()
+                .lines()
+                .flatten()
+                .take_while(|line| line != "OK"),
+        );
+
+        Ok(playlist)
     }
 }

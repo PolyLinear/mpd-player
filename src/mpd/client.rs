72d584ef8423f64f26238a5671455f62f2c461e6
PolyLinear <bciesla@umass.edu>
2025-07-23 21:21:22 -0400

    MPD Stat and Status starter code

--- src/mpd/client.rs
+++ src/mpd/client.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 use std::{
     error::Error,
     io::{BufRead, BufReader, BufWriter, Read, Write},
@@ -24,6 +25,84 @@ macro_rules! lines {
             .flatten()
             .take_while(|line| line != "OK")
     };
+}
+
+pub enum MPDQueueState {
+    Off,
+    On,
+    Oneshot,
+}
+
+impl Default for MPDQueueState {
+    fn default() -> Self {
+        Self::Off
+    }
+}
+
+pub enum MPDState {
+    Play,
+    Stop,
+    Pause,
+}
+
+impl Default for MPDState {
+    fn default() -> Self {
+        Self::Stop
+    }
+}
+
+#[derive(Default)]
+pub struct MPDStatus {
+    partition: Option<String>,
+    volume: Option<u8>,
+    repeat: MPDQueueState,
+    random: MPDQueueState,
+    single: MPDQueueState,
+    consume: MPDQueueState,
+    playlist: Option<u32>,
+    playlistlength: Option<u64>,
+    state: MPDState,
+    song: Option<u32>,
+    songid: Option<u32>,
+    nextsong: Option<u32>,
+    nextsongid: Option<u32>,
+    time: Option<u32>, //In format num:num docs don't tell me what the numbes represent
+    elapsed: Option<f32>,
+    duration: Option<f32>,
+    bitrate: Option<u16>,
+    xfade: Option<u16>,
+    mixramdb: Option<u16>,
+    mixrampdelay: Option<u16>,
+    audio: Option<String>, //TODO samplerate:bits:channels (need to further format this string)
+    //update_db: Option<> not included for now
+    error: Option<String>,
+    lastloadedplaylist: Option<String>,
+}
+
+impl MPDStatus {
+    fn new<T: Iterator>(iter: &mut T) -> Result<Self, MPDError> {
+        let mut status = MPDStatus::default();
+        todo!("Implement MPD STATUS");
+        Ok(status)
+    }
+}
+
+#[derive(Default)]
+pub struct MPDStats {
+    artists: u32,
+    albums: u32,
+    uptime: u32,
+    db_playtime: u64,
+    db_update: u64,
+    playtime: u32,
+}
+
+impl MPDStats {
+    fn new<T: Iterator>(iter: &mut T) -> Result<Self, MPDError> {
+        let mut stats = MPDStats::default();
+        todo!("Implement MPD STATS");
+        Ok(stats)
+    }
 }
 
 impl std::fmt::Display for MPDError {
@@ -92,5 +171,31 @@ impl MPDClient {
         playlist.extend(lines!(self));
 
         Ok(playlist)
+    }
+
+    pub fn clear_queue(&mut self) -> Result<(), MPDError> {
+        self.mpd_command(b"clear\n")?;
+        Ok(())
+    }
+
+    pub fn load_playlist(&mut self, name: &str) -> Result<(), MPDError> {
+        self.mpd_command(format!("load {}\n", name).as_bytes())?;
+        Ok(())
+    }
+
+    pub fn get_status(&mut self) -> Result<Vec<String>, MPDError> {
+        let first = self.mpd_command(b"status\n")?;
+        let mut status_pairs = Vec::with_capacity(16);
+        status_pairs.push(first);
+        status_pairs.extend(lines!(self));
+
+        Ok(status_pairs)
+    }
+
+    pub fn currentsong(&mut self) -> Result<Vec<String>, MPDError> {
+        let _ = self.mpd_command(b"currentsong\n")?;
+        let mut song_pairs = Vec::with_capacity(16);
+        song_pairs.extend(lines!(self));
+        Ok(song_pairs)
     }
 }
